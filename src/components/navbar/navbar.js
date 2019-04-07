f5c5c19c091119b25a3fa5c2231d985fd896e91a
majolex <majolex@matts-MacBook-Air.local>
2019-04-06 22:43:14 -0700

    added react

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,18 +1,19 @@
 import React from "react"
 export default function Navbar() {
     return (
-        <div className="pos-f-t">
-            <div className="collapse" id="navbarToggleExternalContent">
-                <div className="bg-dark p-4">
-                    <h5 className="text-white h4">Collapsed content</h5>
-                    <span className="text-muted">Toggleable via the navbar brand.</span>
-                </div>
-            </div>
-            <nav className="navbar navbar-dark bg-dark">
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-            </nav>
-        </div>
+        <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Map</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Volunteer</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
     )
 }
