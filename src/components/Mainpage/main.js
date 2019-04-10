d7c40c05c7e505fb9431e9e4ba7e7dc6fab3e1c0
majolex <majolex@matts-MacBook-Air.local>
2019-04-10 16:06:25 -0700

    added things

---
+++ src/components/Mainpage/main.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./navbar";
+
+export default function Mainpage() {
+    return(
+        
+        <ImageBackground >
+    
+        
+        
+        </ImageBackground>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </div>)
+}
