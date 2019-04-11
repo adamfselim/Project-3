618ca745441ebee5cc39e19e45c5487eb23068a7
majolex <majolex@matts-MacBook-Air.local>
2019-04-10 17:17:04 -0700

    added final navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,22 +2,26 @@ import React from "react";
 import "./navbar.css";
 
 export default function Navbar() {
-    return (<div>
-      <h1>Gleam Globe</h1>
-        <ul id="navbackground" className="nav justify-content-center">
-        <li className="nav-item">
-          <a className="nav-link active navwhite"  href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link navwhite" href="#">Map</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link navwhite" href="#">Volunteer</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link navwhite" href="#">Contact</a>
-        </li>
-      </ul>
-      </div>
-    )
+  return (<div>
+    
+    <ul id="navbackground" class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link active navwhite container" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link navwhite" href="#">Volunteer</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link navwhite" href="#">Map</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link navwhite" href="#">Contact</a>
+      </li>
+    </ul>
+    <h1>Gleam Globe</h1>
+    <backgroundimage>
+
+    </backgroundimage>
+  </div>
+  )
 }
