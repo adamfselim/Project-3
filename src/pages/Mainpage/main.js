64df1684f226cd26697e8d9d0ed05fa25d553938
majolex <majolex@matts-MacBook-Air.local>
2019-04-10 21:28:53 -0700

    added things

--- src/pages/Mainpage/main.js
+++ src/pages/Mainpage/main.js
@@ -2,15 +2,8 @@ import React from "react";
 import "./navbar";
 
 export default function Mainpage() {
-    return(
-        
-        <ImageBackground >
-    
-        
-        
-        </ImageBackground>
-        
-        
+    return(<div>
+       
         
         
         
