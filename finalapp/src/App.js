00f41b02a6ab7771ad70a196f8ad4b488c39693e
clagarda <clagarda2390@yahoo.comclagarda2390@yahoo.comgit config --global user.email clagarda2390@yahoo.com>
2019-04-13 10:31:02 -0700

    starting contact page

--- finalapp/src/App.js
+++ finalapp/src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  
+  return (
+    <div class ="container">
+      <adress>
+        <h3>Contact Team Awesome!</h3>
+        
+      </adress>
+    </div>
+  )
 }
 
 export default App;
