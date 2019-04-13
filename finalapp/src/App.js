6169b2fb0f5658e91fe1d1ca27c5df91d07d02f6
clagarda <clagarda2390@yahoo.comclagarda2390@yahoo.comgit config --global user.email clagarda2390@yahoo.com>
2019-04-13 12:16:44 -0700

    basic contact page

--- finalapp/src/App.js
+++ finalapp/src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div class ="container">
-      <adress>
-        <h3>Contact Team Awesome!</h3>
-        
-      </adress>
+    <div class ="card">
+      <div class="card-body">
+        <adress id ="addy">
+          <h3>Contact Team Awesome!</h3>
+          Email us at <a href="mailto:savetheearth@gmail.com">savetheearth@gmail.com</a>.<br></br>
+          348 N Oak Cir<br></br>
+          Gilbert, AZ 85233<br></br>
+          United States<br></br>
+        </adress>
+      </div>
     </div>
   )
 }
