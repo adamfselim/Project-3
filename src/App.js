21b5b5322912497c06b9fcbde4f5f0cdc5a01330
majolex <majolex@matts-MacBook-Air.local>
2019-04-13 14:18:49 -0700

    added nav and images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import logo from './logo.svg';
 import Navbar from "./components/navbar/navbar";
 import Mainpage from './pages/Mainpage/main';
 import Volunteer from './pages/volunteer/volunteer';
@@ -9,6 +9,8 @@ import Contact from './pages/contact/contact';
 import Wastereduction from './pages/wastereduction/waste';
 import Calendar from "./components/calendar/Calendar";
 import "./App.css";
+import wastereduction from "./components/images/earthmonth.png";
+
 
 
 //new calendar component code
@@ -17,24 +19,26 @@ import "./App.css";
 class App extends React.Component {
   render() {
     return (
-     <Router>
-      <div className="App">
-
-       <Navbar />
-        <Switch>
-        <Route exact path="/" component={Mainpage} />
-        <Route exact path="/volunteer" component={Volunteer} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/map" component={Map} />
-        <Route exact path="/wastereduction" component={Wastereduction} />
-        </Switch>
-       
-        <main>
-          <Calendar />
-        </main>
-
-      </div>
- </Router>
+      <Router>
+        <div className="App">
+          <div>
+            
+            <div className="imagecard col-md-8 mx-auto">
+            
+            </div>
+            
+            <div className="bg-dark text-white">
+              <Switch>
+                <Route exact path="/" component={Mainpage} />
+                <Route exact path="/volunteer" component={Volunteer} />
+                <Route exact path="/contact" component={Contact} />
+                <Route exact path="/map" component={Contact} />
+                <Route exact path="/wastereduction" component={Wastereduction} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
--- src/pages/volunteer/volunteer.js
+++ src/pages/volunteer/volunteer.js
@@ -1,14 +1,23 @@
 import React from "react";
-import "../../components/navbar/navbar";
 // import "../../components/images/";
 import "./volunteer.css";
 import Card from "../../components/card/card";
-
+import Navbar from '../../components/navbar/navbar';
+import volunteerImg from '../../components/images/recycle-20525_640.jpg';
 
 export const Volunteer = ()  => {
    return(
-           <Card />
-       );
+        <div className="Overlay"  style={{backgroundImage:`url(${volunteerImg})`}}>
+            <Navbar/>
+            <h1>Dude wat?</h1>
+            <p>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+            </p>
+
+            <Card />
+        </div>
+    );
   
 };
 
