38305d1f03e595cc93803f1de9c0826699a98699
clagarda <clagarda2390@yahoo.comclagarda2390@yahoo.comgit config --global user.email clagarda2390@yahoo.com>
2019-04-17 20:31:20 -0700

    updating files

---
+++ projectapp/src/contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './contact.css';
+
+function navbar() {
+    return (
+      <div class ="card">
+      <div class="card-body">
+        <address id ="addy">
+          <h3>Contact Team Awesome!</h3>
+          Email us at <a href="mailto:savetheearth@gmail.com">savetheearth@gmail.com</a>.<br></br>
+          348 N Oak Cir<br></br>
+          Gilbert, AZ 85233<br></br>
+          United States<br></br>
+        </address>
+      </div>
+    </div>
+  )
+}
+
+
+export default navbar;
--- projectapp/src/index.js
+++ projectapp/src/index.js
@@ -4,8 +4,9 @@ import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import App from './App';
+
+
 
 
 
