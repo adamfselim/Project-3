b2f025b45bf4f1c454488e917e541e6ad5d50dac
majolex <majolex@matts-MacBook-Air.local>
2019-04-18 16:23:18 -0700

    added constact

--- src/pages/Mainpage/main.js
+++ src/pages/Mainpage/main.js
@@ -2,8 +2,7 @@ import React from "react";
 import "../../components/navbar/navbar";
 import "../../components/images/Nature.jpg"
 import "./style.css";
-import Navbar from '../../components/navbar/navbar';
-import mainImg from '../../components/images/Nature.jpg';
+import mainImg from "../../components/images/Nature.jpg";
 import Card from "../../components/card/card";
 
 export const Main = ()  => {
