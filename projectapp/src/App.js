92e1781d50d6a3021f05694c2ef381c56face4ee
clagarda <clagarda2390@yahoo.comclagarda2390@yahoo.comgit config --global user.email clagarda2390@yahoo.com>
2019-04-17 18:43:28 -0700

    navbar from home page

--- projectapp/src/App.js
+++ projectapp/src/App.js
@@ -5,17 +5,18 @@ import './App.css';
 function App() {
   return (
     <div class ="card">
-      <div class="card-body">
-        <adress id ="addy">
-          <h3>Contact Team Awesome!</h3>
-          Email us at <a href="mailto:savetheearth@gmail.com">savetheearth@gmail.com</a>.<br></br>
-          348 N Oak Cir<br></br>
-          Gilbert, AZ 85233<br></br>
-          United States<br></br>
-        </adress>
-      </div>
+    <div class="card-body">
+      <adress id ="addy">
+        <h3>Contact Team Awesome!</h3>
+        Email us at <a href="mailto:savetheearth@gmail.com">savetheearth@gmail.com</a>.<br></br>
+        348 N Oak Cir<br></br>
+        Gilbert, AZ 85233<br></br>
+        United States<br></br>
+      </adress>
     </div>
+  </div>
   )
 }
+
 
 export default App;
