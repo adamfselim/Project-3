14ba152893fe5ab9a0e8d1221a7d20d36528a7ea
clagarda <clagarda2390@yahoo.comclagarda2390@yahoo.comgit config --global user.email clagarda2390@yahoo.com>
2019-04-20 13:10:26 -0700

    updated contact page

--- projectapp/src/pages/contact/contact.js
+++ projectapp/src/pages/contact/contact.js
@@ -9,7 +9,7 @@ export const Contact = () => {
     return (
         <React.Fragment>
           <div id='backgroundimage4' className="overlay"  style={{backgroundImage:`url(${contactImg})`}}></div>
-          <div id="contentCard"><Card cardImage="contactImg"/>
+          <div id="contentCard"><Card cardImage="contactImg">            
             <adress id ="addy">
               <h3>Contact Team Awesome!</h3>
               Email us at <a href="mailto:savetheearth@gmail.com">savetheearth@gmail.com</  a>.<br></br>
@@ -17,6 +17,7 @@ export const Contact = () => {
               Gilbert, AZ 85233<br></br>
               United States<br></br>
             </adress>
+            </Card>
           </div>
           
         </React.Fragment>
