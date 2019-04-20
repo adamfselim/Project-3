0bd63f635990a5feba26d3c74ae715b743bc61a8
Edward Adam Farris <edwardadamfarris@Edwards-MacBook-Pro.local>
2019-04-20 10:28:45 -0700

    removed some files to reduce conflicts

--- src/pages/Mainpage/main.js
+++ src/pages/Mainpage/main.js
@@ -1,27 +1,2 @@
-import React from "react";
-import "../../components/navbar/navbar";
-import "../../components/images/person.jpg"
-import "./style.css";
-import Navbar from '../../components/navbar/navbar';
-import mainImg from '../../components/images/person.jpg';
-import Card from "../../components/card/card";
-
-export const Main = ()  => {
-   return(
-        <div className="Overlay"  style={{backgroundImage:`url(${mainImg})`}}>
-            <Navbar/>
-            <h1>Dude wat?</h1>
-            <p>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
-            </p>
-
-           <Card />
-        </div>
-    );
-  
-};
-
-export default Main; 
 
 
