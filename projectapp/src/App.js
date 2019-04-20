e1ccc7f62f7b2e7d1c882b54380e6b10ef21f674
clagarda <clagarda2390@yahoo.comclagarda2390@yahoo.comgit config --global user.email clagarda2390@yahoo.com>
2019-04-20 10:23:06 -0700

    cleaned up files

--- projectapp/src/App.js
+++ projectapp/src/App.js
@@ -4,28 +4,17 @@ import './App.css';
 
 function App() {
   return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" id ="home" href="home">Welcome</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="volunteer">Volunteer<span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link" href="map">Map</a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link" href="contact">Contact</a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link" href="recycle">Waste/Recycle</a>
-          </li>
-        </ul>
-        </div>
-      </nav>
+    <div class ="card">
+    <div class="card-body">
+      <adress id ="addy">
+        <h3>Contact Team Awesome!</h3>
+        Email us at <a href="mailto:savetheearth@gmail.com">savetheearth@gmail.com</a>.<br></br>
+        348 N Oak Cir<br></br>
+        Gilbert, AZ 85233<br></br>
+        United States<br></br>
+      </adress>
+    </div>
+  </div>
   )
 }
 
