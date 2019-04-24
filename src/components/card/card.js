e1f58ab6d5efba414d0ad3ddf2e2d0c9cc4834c7
Duane Dibbley <44147755+cjenne@users.noreply.github.com>
2019-04-23 22:35:53 -0700

    fixed opacity for map and form

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -10,7 +10,7 @@ export const Card = (props) => (
             <Navbar />
         </div>
 
-        <div className="bg-white text-black cardtext">
+        <div>
             <div>
                 <div> {props.children}
                 </div>
