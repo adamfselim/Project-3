572def84c7626e44a0a4344ff403f452a1cd79a5
Duane Dibbley <44147755+cjenne@users.noreply.github.com>
2019-04-23 18:52:01 -0700

    update

--- src/components/map/MapContainer.js
+++ src/components/map/MapContainer.js
@@ -3,9 +3,13 @@ import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
 import CurrentLocation from './Map';
 
-// const divImageStyle = {
-//   float:left
-// };
+const divImageStyle = {
+  float: 'left'
+};
+const divInfoStyle = {
+  float: 'right',
+  padding: '10px'
+};
 
 export class MapContainer extends Component {
   state = {
@@ -27,13 +31,13 @@ export class MapContainer extends Component {
         showingInfoWindow: false,
         activeMarker: null
       });
-  // onMapClicked = () => {
-  //   if (this.state.showingInfoWindow)
-  //     this.setState({
-  //        activeMarker: null,
-  //       showingInfoWindow: false
-  //      });
-  //     };
+      // onMapClicked = () => {
+      //   if (this.state.showingInfoWindow)
+      //     this.setState({
+      //        activeMarker: null,
+      //       showingInfoWindow: false
+      //      });
+      //     };
     }
   };
 
@@ -43,14 +47,18 @@ export class MapContainer extends Component {
         // className="map"
         centerAroundCurrentLocation
         google={this.props.google}
-        // onClick={this.onMapClicked}
+      // onClick={this.onMapClicked}
       >
         <Marker
-          onClick={this.onMarkerClick} name= {'Current Location.'}/>
-
-        {/* <Marker {this.state.markerArray.map(marker => (
+          onClick={this.onMarkerClick} name={'Current Location.'} />
+        <Marker
+          onClick={this.onMarkerClick} name={'Title.'} position={{ lat: 33.349228, lng: -111.796241 }} image={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT66lw7_QqeRs0bf3UH_QFEblAwV0vpZatBgCds1mflDjdUGfGR'} address={'123 Fake St'} city={'Phoenix'} state={'AZ'} date={'05/09/2019'} time={'08:00'}/>
+        {/*{this.state.event.map( Tevent => (
             <Marker 
-            position={marker.location}
+            key={Tevent.id}
+            position={{ lat: {Tevent.lat}, lng: {Tevent.lng} }}
+            date={Tevent.date}
+            
             />
           ))}/> */}
 
@@ -59,21 +67,30 @@ export class MapContainer extends Component {
           visible={this.state.showingInfoWindow}
           onClose={this.onClose}>
           <div>
-          {/* <div {divImageStyle}><img src='http://i.stack.imgur.com/g672i.png'></div><div style='float:right; padding: 10px;'><b>Title</b><br/>123 Address<br/> City,Country</div> */}
-            <h4>{this.state.selectedPlace.name}</h4>
+            <div style={divImageStyle}>
+              <img src={this.state.selectedPlace.image}/>
+            </div>
+            <div style={divInfoStyle}>
+              <h4>{this.state.selectedPlace.name}</h4>
+              <p>{this.state.selectedPlace.date}</p>
+              <p>{this.state.selectedPlace.time}</p>
+              <p>{this.state.selectedPlace.address}</p>
+              <p>{this.state.selectedPlace.city}</p>
+              <p>{this.state.selectedPlace.state}</p>
+            </div>
           </div>
         </InfoWindow>
-        {/* <InfoWindow visible>
+          {/* <InfoWindow visible>
           <small>
             Click on any of the markers to display an additional info.
           </small>
         </InfoWindow> */}
       </CurrentLocation>
-    );
-  }
-}
+        );
+      }
+    }
 
 export default GoogleApiWrapper({
-  apiKey: (process.env.REACT_APP_API_KEY_YT)
-})(MapContainer);
-
+          apiKey: (process.env.REACT_APP_API_KEY_YT)
+    })(MapContainer);
+    
