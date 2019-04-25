580e7eaf737eed354408e8b38f09347f27e27073
Duane Dibbley <44147755+cjenne@users.noreply.github.com>
2019-04-24 18:01:33 -0700

    get commands

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 
 export default {
     // Gets all books
-    // getBooks: function() {
-    //   return axios.get("/api/books");
-    // },
-    // // Gets the book with the given id
-    // getBook: function(id) {
-    //   return axios.get("/api/books/" + id);
-    // },
+    getEvents: function() {
+      return axios.get("/api/events");
+    },
+    // Gets the book with the given id
+    getEvent: function(id) {
+      return axios.get("/api/events/" + id);
+    },
     // // Deletes the book with the given id
-    // deleteBook: function(id) {
-    //   return axios.delete("/api/books/" + id);
-    // },
+    deleteEvent: function(id) {
+      return axios.delete("/api/events/" + id);
+    },
     // Saves a book to the database
     saveEvent: function(eventData) {
       console.log("API.js", eventData);
