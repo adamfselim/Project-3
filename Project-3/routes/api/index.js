ef5b413976f04b7f5f931c958ccda722e136fc1d
Justin Fredricksen <fredricksenjustin@gmail.com>
2019-04-27 15:54:51 -0700

    clearing branch

--- Project-3/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const eventRoutes = require("./events");
-const signinRoutes = require("./signin");
-
-// Event routes
-router.use("/events", eventRoutes);
-router.use("/users", signinRoutes);
-
-module.exports = router;
